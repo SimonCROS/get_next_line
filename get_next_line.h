483d2ef7a6bf3ebe079dba0615bf61f1e58cee50
Simon Cros <scros@student.42lyon.fr>
2020-12-02 17:46:13 +0100

    Norme .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: scros <scros@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 13:45:04 by scros             #+#    #+#             */
-/*   Updated: 2020/12/02 17:22:52 by scros            ###   ########lyon.fr   */
+/*   Updated: 2020/12/02 17:45:52 by scros            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#include <stdio.h>
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
-size_t	ft_strlen(const char *s);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	**ft_split_first(char *s, char c);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strchr(const char *s, int c);
-int		get_next_line(int fd, char **line);
+size_t				ft_strlen(const char *s);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+char				**ft_split_first(char *s, char c);
+char				*ft_substr(const char *s, unsigned int start, size_t len);
+char				*ft_strjoin(const char *s1, const char *s2);
+char				*ft_strchr(const char *s, int c);
+int					get_next_line(int fd, char **line);
 
 #endif
